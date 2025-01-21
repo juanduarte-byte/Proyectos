99628faf53ba244dabf6147f9aa8f5f01de96c74
juanduarte-byte <juanduarte2317@gmail.com>
2025-01-21 12:01:18 -0600

    tareas

---
+++ CalculadoraBásica/Program.cs
@@ -0,0 +1,118 @@
+﻿string? leeRespuesta;
+
+//Variable donde se guardarán lo ingresado por el usuario
+string respuesta = "";
+
+double primerNumero = 0.0;
+double segundoNumero = 0.0;
+double sumaNumeros = 0.0;
+double restaNumeros = 0.0;
+double multiplicacionNumeros = 0.0;
+double divisionNumeros = 0.0;
+
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Calculadora Básica\n");
+
+    /*Se dan dos instrucciones y para ingresar el primer número después valída su respuesta para pasarlo a una variable 
+    declarado como entero: primerNumero. TENIENDO EN CUENTA QUE SOLO INGRESA NUMEROS*/
+    Console.WriteLine("Ingrese dos números");
+    Console.WriteLine("Ingrese el primero:");
+    leeRespuesta = Console.ReadLine();
+    if (leeRespuesta != null)
+    {
+        primerNumero = double.Parse(leeRespuesta);
+    }
+
+    /*Segunda instrucción para ingresar el segundo número y validar lo ingresado*/
+    Console.WriteLine("Ingrese el segundo:");
+    leeRespuesta = Console.ReadLine();
+    if (leeRespuesta != null)
+    {
+        segundoNumero = double.Parse(leeRespuesta);
+    }
+
+    /*Después de ingresar los dos números se imprimen las operaciones a realizar y valida lo ingresado por el usuario*/
+    Console.WriteLine("Elige la operación de desee que realice ingresando el número de la operación, si desea cambiar el número de clic en enter\n");
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicación");
+    Console.WriteLine("4. División");
+
+    leeRespuesta = Console.ReadLine();
+    if (leeRespuesta != null)
+    {
+        respuesta = leeRespuesta;
+    }
+
+    /*Teniendo en cuenta el menú, con el switch case se evalúa lo ingresado contemplando las opciones posibles para la 
+    realización de las operaciones*/
+    switch (respuesta)
+    {
+        case "1":
+            Console.WriteLine();
+            sumaNumeros = primerNumero + segundoNumero;
+            Console.WriteLine($"{primerNumero} + {segundoNumero} = {sumaNumeros}\n");
+
+            //Este código considera si desea continuar o salir del programa. Se utiliza en los cuatro casos
+            Console.WriteLine("Si desea continuar de clic en Enter, de lo contrario escriba Exit");
+            leeRespuesta = Console.ReadLine();
+            if (leeRespuesta != null)
+            {
+                respuesta = leeRespuesta.ToLower();
+            }
+            break;
+        case "2":
+            Console.WriteLine();
+            restaNumeros = primerNumero - segundoNumero;
+            Console.WriteLine($"{primerNumero} - {segundoNumero} = {restaNumeros}\n");
+
+            Console.WriteLine("Si desea continuar de clic en Enter, de lo contrario escriba Exit");
+            leeRespuesta = Console.ReadLine();
+            if (leeRespuesta != null)
+            {
+                respuesta = leeRespuesta.ToLower();
+            }
+            break;
+        case "3":
+            Console.WriteLine();
+            multiplicacionNumeros = primerNumero * segundoNumero;
+            Console.WriteLine($"{primerNumero} * {segundoNumero} = {multiplicacionNumeros}\n");
+
+            Console.WriteLine("Si desea continuar de clic en Enter, de lo contrario escriba Exit");
+            leeRespuesta = Console.ReadLine();
+            if (leeRespuesta != null)
+            {
+                respuesta = leeRespuesta.ToLower();
+            }
+            break;
+        case "4":
+            Console.WriteLine();
+
+            //Antes de continuar la división se evaluán los casos de división por cero para evitar errores
+            if (primerNumero == 0 && segundoNumero == 0)
+            {
+                Console.WriteLine("Resultado indefinido");
+            }
+            else if (primerNumero > 0 && segundoNumero == 0)
+            {
+                Console.WriteLine("No se puede dividir entre cero");
+            }
+            else
+            {
+                divisionNumeros = primerNumero / segundoNumero;
+                Console.WriteLine($"{primerNumero} / {segundoNumero} = {divisionNumeros}\n");
+            }
+
+            Console.WriteLine("Si desea continuar de clic en Enter, de lo contrario escriba Exit");
+            leeRespuesta = Console.ReadLine();
+            if (leeRespuesta != null)
+            {
+                respuesta = leeRespuesta.ToLower();
+            }
+            break;
+    }
+
+} while (respuesta != "exit");
---
+++ PromedioDeNumeros/Program.cs
@@ -0,0 +1,69 @@
+﻿//PromedioDeNumeros
+//Se declaran variables para leer las respuestas del usuario y pasarla a otra variable.
+string? leerRespuesta;
+string respuesta = "";
+
+//Variable que valida el número ingresado
+bool validarNumero = false;
+
+// Se usa este bucle con el fin de que todo se valide y si quiere salir del programa
+do
+{
+    Console.Clear();
+
+    //Se declaran e inicializan variables para la realización de la suma y también el promedio
+    int numero = 0;
+    int sumaNumeros = 0;
+    int cantidadDeNumeros = 0;
+
+    double promedio = 0.00;
+
+    Console.WriteLine("Ingrese la cantidad de números a sumar y promediar:");
+    leerRespuesta = Console.ReadLine();
+
+    /* Si cumple que es diferente de null entonces procede a tratar de convertirlo a un entero, validarlo y realizar la suma con
+    el bucle for*/
+    if (leerRespuesta != null)
+    {
+        /*Se trata de convertir la variable leerRespuesta en un número y arroja true o false en validarNumero*/
+        validarNumero = int.TryParse(leerRespuesta, out cantidadDeNumeros);
+        
+        //Si es un entero entonces procede a realizar la suma con números enteros, luego promediar para imprimir
+        if (validarNumero)
+        {
+            Console.WriteLine("Ingrese los números: ");
+
+            for (int i = 0; i < cantidadDeNumeros; i++)
+            {
+                do
+                {
+                    leerRespuesta = Console.ReadLine();
+                    if (leerRespuesta != null)
+                    {
+                        validarNumero = int.TryParse(leerRespuesta, out numero);
+                    }
+
+                } while (validarNumero == false);
+
+                sumaNumeros += numero;
+            }
+
+            promedio = sumaNumeros / cantidadDeNumeros;
+            Console.WriteLine($"\nEl resultado de la suma es: {sumaNumeros}");
+            Console.WriteLine($"El promedio es: {promedio}");
+        }
+        // Este código es una opción si no se pudo convertir cantidadNumero como un entero y vuelva a iniciar y poner todo bien de nuevo.
+        else
+        {
+            Console.WriteLine("Ocurrió un error, lo ingresado no es un número entero");
+        }
+    }
+
+    Console.WriteLine("De clic en enter para ir al inicio o escriba Exit si desea salir del programa");
+    leerRespuesta = Console.ReadLine();
+    if (leerRespuesta != null)
+    {
+        respuesta = leerRespuesta.ToLower();
+    }
+
+} while (respuesta != "exit");
