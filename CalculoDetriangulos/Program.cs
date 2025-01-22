6e92bba3834942bac7a137e08f9da5307fda6e5e
juanduarte-byte <juanduarte2317@gmail.com>
2025-01-21 21:49:35 -0600

    tareas

--- CalculoDetriangulos/Program.cs
+++ CalculoDetriangulos/Program.cs
@@ -1,7 +1,9 @@
-﻿string? leerRespuesta;
+﻿//Se declaran variables que serán utiles
+string? leerRespuesta;
 string respuesta = "";
 string tipoDeTriangulo = "";
 
+//Utilizamos un bucle do para que siga realizando lo mismo en los casos
 do
 {
     double ladoA = 0.00;
@@ -45,12 +47,15 @@ do
         tipoDeTriangulo = "Escaleno";
     }
 
+    //Se imprime los resultados del triangulo y también el área
     Console.WriteLine();
     Console.WriteLine($"El triángulo es un {tipoDeTriangulo}");
     Console.WriteLine($"El área del triángulo {tipoDeTriangulo} es: {CalcularArea(ladoA, ladoB, ladoC)}\n");
 
+    //Se le pregunta al usuario si desea continuar o no
     Console.WriteLine("Si desea continuar de clic en enter, de lo contrario escriba Exit");
     leerRespuesta = Console.ReadLine();
+    //Se validan los valores
     if (leerRespuesta != null)
     {
         respuesta = leerRespuesta.ToLower();
@@ -58,12 +63,14 @@ do
 
 } while (respuesta != "exit");
 
+//Se utiliza un método con el fin de hacer el cálculo Area
 double CalcularArea(double ladoA, double ladoB, double ladoC)
 {
     double semiPerimetro = 0.00;
     double calculoParaArea = 0.00;
     double areaDelTriangulo = 0.00;
 
+    //También se imprime 
     semiPerimetro = (ladoA + ladoB + ladoC) / 2;
     calculoParaArea = (semiPerimetro) * (semiPerimetro - ladoA) * (semiPerimetro - ladoB) * (semiPerimetro - ladoC);
     return areaDelTriangulo = Math.Sqrt(calculoParaArea);
