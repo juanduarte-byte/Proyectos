15d46063f05ba33a86a615759fa5413e208595e6
juanduarte-byte <juanduarte2317@gmail.com>
2025-01-21 19:21:31 -0600

    tareas

---
+++ MultiplicacionNumeroEntero/Program.cs
@@ -0,0 +1,20 @@
+﻿string? leeResultado;
+
+int numeroEntero = 0;
+int resultado = 0;
+
+bool validaNumero = false;
+
+Console.WriteLine("Programa que multiplica un número entero por 2");
+do
+{
+    Console.Write("Ingresa un número entero: ");
+    leeResultado = Console.ReadLine();
+    if (leeResultado != null)
+    {
+        validaNumero = int.TryParse(leeResultado, out numeroEntero);
+    }
+} while (validaNumero != true);
+
+resultado = 2 * numeroEntero;
+Console.WriteLine($"\n2 * {numeroEntero} = {resultado}");
