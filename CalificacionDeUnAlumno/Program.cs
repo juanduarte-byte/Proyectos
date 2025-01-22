e8cffd2a4fec8c59e0d766f40cc0be9610f8efcd
juanduarte-byte <juanduarte2317@gmail.com>
2025-01-21 18:30:58 -0600

    tareas

---
+++ CalificacionDeUnAlumno/Program.cs
@@ -0,0 +1,37 @@
+﻿string? leeResultado;
+
+double puntajePracticas = 0.00;
+double puntajeExamen = 0.00;
+double puntajeTareas = 0.00;
+double calificacionFinal = 0.00;
+
+Console.WriteLine("Ingrese las tres actividades del alumno (Prácticas) (Examen) (Tareas)");
+
+Console.Write("Prácticas: ");
+leeResultado = Console.ReadLine();
+if (leeResultado != null)
+{
+    puntajePracticas = double.Parse(leeResultado);
+    puntajePracticas *= 0.55;
+}
+
+Console.Write("Examen: ");
+leeResultado = Console.ReadLine();
+if (leeResultado != null)
+{
+    puntajeExamen = double.Parse(leeResultado);
+    puntajeExamen *= 0.30;
+}
+
+Console.Write("Tareas: ");
+leeResultado = Console.ReadLine();
+if (leeResultado != null)
+{
+    puntajeTareas = double.Parse(leeResultado);
+    puntajeTareas *= 0.15;
+}
+
+calificacionFinal = puntajePracticas + puntajeExamen + puntajeTareas;
+
+Console.Write($"La calificación final del alumno es de: {calificacionFinal}");
+Console.WriteLine();
