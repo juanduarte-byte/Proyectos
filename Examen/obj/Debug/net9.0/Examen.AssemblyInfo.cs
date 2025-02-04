d5b5637fdba35ca6673086ed94e0e1b8824f5ddc
juanduarte-byte <juanduarte2317@gmail.com>
2025-02-04 13:18:28 -0600

    tareas

--- Examen/obj/Debug/net9.0/Examen.AssemblyInfo.cs
+++ Examen/obj/Debug/net9.0/Examen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac38ef002ff56cc12a06393a397fe7d531e705e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4df7baab757895d26509fca4f32b966d74bae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
