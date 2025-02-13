0462f06f40f6cf94d9bc982bc3b53dc1f479a409
juanduarte-byte <juanduarte2317@gmail.com>
2025-02-12 18:56:40 -0600

    tareas

---
+++ Calculo_De_Agua_m3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AguaEnLitros
+{
+    public class Agua
+    {
+        public double ConverirEnLitros(double aguaMetrosCubic)
+        {
+            return aguaMetrosCubic * 1000;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la cantidad de agua que está en metros cúbicos durante el mes");
+            double aguaMetrosCubic = Convert.ToDouble(Console.ReadLine());
+
+            Agua agua = new Agua();
+            double cantidadDeAgua = agua.ConverirEnLitros(aguaMetrosCubic);
+
+            Console.WriteLine($"La cantidad de agua que consume durante el mes en litros es de: {cantidadDeAgua} L");
+        }
+    }
+}
